b4ccbc6537119fe37b5a31ca373ed5d076dc6e55
Webasif1 <mdasif606392@gmail.com>
2026-05-02 03:32:38 +0600

    implement models for tickets, workspaces, agents, and company admins

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 // import files and modules
-import userModel from "../models/user.model.js";
-import { HTTP_STATUS, ERROR_MESSAGES } from "../config/constants.js";
-import { AppError, asyncHandler } from "../utils/errorHandler.js";
-import { generateToken } from "../utils/tokens.js";
-import { sendVerificationEmail } from "../utils/email.js";
-import { config } from "../config/config.js";
+import userModel from '../models/user.model.js';
+import { HTTP_STATUS, ERROR_MESSAGES } from '../config/constants.js';
+import { AppError, asyncHandler } from '../utils/errorHandler.js';
+import { generateToken } from '../utils/tokens.js';
+import { sendVerificationEmail } from '../utils/email.js';
+import { config } from '../config/config.js';
 
-const recieverEmail = "huzaifaquadri1853@gmail.com";
+const recieverEmail = 'huzaifaquadri1853@gmail.com';
 
 // ============================================
 // Register User
@@ -23,13 +23,13 @@ export const registerController = asyncHandler(async (req, res) => {
     email,
     password,
     fullName,
-    role,
+    role
   });
   const userResponse = {
     id: user._id,
     email: user.email,
     fullName: user.fullName,
-    role: user.role,
+    role: user.role
   };
 
   console.log(`✅ New user registered: ${user.email}`);
@@ -38,9 +38,9 @@ export const registerController = asyncHandler(async (req, res) => {
 
   res.status(HTTP_STATUS.CREATED).json({
     success: true,
-    message: "User registered successfully",
+    message: 'User registered successfully',
     data: userResponse,
-    token: token,
+    token: token
   });
 
   const sent = await sendVerificationEmail({
@@ -49,12 +49,12 @@ export const registerController = asyncHandler(async (req, res) => {
     name: user.name,
     verificationLink:
       process.env.backendURL ||
-      `http://localhost:${config.PORT}/api/auth/verify/${token}`,
+      `http://localhost:${config.PORT}/api/auth/verify/${token}`
   });
   if (sent) {
-    console.log("📧 Verification Email sent successfully");
+    console.log('📧 Verification Email sent successfully');
   } else {
-    console.log("❎ Verification Email not sent");
+    console.log('❎ Verification Email not sent');
   }
 });
 
